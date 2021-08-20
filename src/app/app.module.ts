bc2667b06484ed38d2c779ed17cd1537be2139f0
Paulo Cruz <paulocac_1@hotmail.com>
2021-08-20 16:17:49 +0100

    salvando usuario no localstorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { FuncionariosService } from '../services/domain/funcionarios.service';
 import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
+import { StorageService } from '../services/storage.service';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { AuthService } from '../services/auth.service';
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     FuncionariosService,
     ErrorInterceptorProvider,
-    AuthService
+    AuthService,
+    StorageService
   ]
 })
 export class AppModule {}
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,11 +2,13 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { API_CONFIG } from "../config/api.config";
 import { CredenciaisDTO } from "../models/credenciais.dto";
+import { LocalUser } from "../models/local_user";
+import { StorageService } from "./storage.service";
 
 @Injectable()
 export class AuthService {
 
-    constructor(public http:HttpClient){
+    constructor(public http:HttpClient, public storageService:StorageService){
 
     }
     
@@ -15,6 +17,7 @@ export class AuthService {
         //User temporario
         if(creds.username == "admin" && creds.password == "123")
         {
+            this.sucessfulLogin(creds.username);
             return true;
         }else{
             return false;
@@ -28,6 +31,18 @@ export class AuthService {
                 observe: 'response',
                 responseType: 'text'
             });*/
+    }
+
+    sucessfulLogin(mail: string){
+        let usr : LocalUser = {
+            email: mail
+        }
+
+        this.storageService.setLocalUser(usr);
+    }
+
+    logout(){
+        this.storageService.setLocalUser(null);
     }
 
 }
